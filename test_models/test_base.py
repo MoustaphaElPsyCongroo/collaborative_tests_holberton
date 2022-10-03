2d2bae0ae8231bdd912c262c77602c4347d999ee
noemiepham <huynhtieuquy@gmail.com>
2022-10-03 21:58:16 +0200

    update test case infinity, string

--- test_models/test_base.py
+++ test_models/test_base.py
@@ -17,10 +17,15 @@ class TestBase(unittest.TestCase):
         b = Base()
         self.assertEqual(b.id, 1)
 
-    def test_float(self):
-        "Test of Base for input float"
-        b = Base(9.5)
-        self.assertEqual(b.id, 9.5)
+    def test_float_inf(self):
+        """Test of Base for input case float infinity"""
+        b = Base(float('inf'))
+        self.assertEqual((b.id, float('inf')))
+
+    def test_string(self):
+        """Test of Base for case input is string"""
+        b = Base("string")
+        self.assertEqual(b.id, "string")
 
 if __name__ == "__main__":
     unittest.main()
