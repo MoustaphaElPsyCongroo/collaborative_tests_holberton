52ec8c1592bef0a5fe9bace566a67dd7d7e46473
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-10-03 12:22:24 +0200

    Add tests for Base and Rectangle instances

---
+++ test_models/test_base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"Unit tests for Base class"
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    "Unit tests suite for Base class"
+
+    def test_constantId(self):
+        "Test of Base for correctly initializing an id"
+        b = Base(5)
+        self.assertEqual(b.id, 5)
+
+    def test_autoId(self):
+        "Test of Base for automatically assigning and incrementing an id"
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
