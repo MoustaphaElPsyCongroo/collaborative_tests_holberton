5ea7635a8cfcb6fb4bbf71a4766c7e46963a666e
ChongLeangUENG <leangcnrp777@gmail.com>
2022-10-12 09:32:57 +0200

    Update test users

--- AirBnB_clone/tests/test_models/test_user.py
+++ AirBnB_clone/tests/test_models/test_user.py
@@ -2,6 +2,7 @@
 """
 Unittest for user.py
 """
+from opcode import hasconst
 import unittest
 from models.user import User
 import datetime
@@ -10,12 +11,22 @@ import datetime
 class UserCase(unittest.TestCase):
     """s"""
 
-    u = User()
+    user = User()
 
     def test_class_exists(self):
         """tests if class exists"""
-        self.assertEqual(str(type(self.u)), "<class 'models.user.User'>")
+        self.assertEqual(str(type(self.user)), "<class 'models.user.User'>")
 
     def test_user_inheritance(self):
         """test if User is a subclass of BaseModel"""
-        self.assertIsInstance(self.u, User)
+        self.assertIsInstance(self.user, User)
+
+    def testHasAttributes(self):
+        """verify if attributes exist"""
+        self.assertTrue(hasattr(self.user, 'email'))
+        self.assertTrue(hasattr(self.user, 'password'))
+        self.assertTrue(hasattr(self.user, 'first_name'))
+        self.assertTrue(hasattr(self.user, 'last_name'))
+        self.assertTrue(hasattr(self.user, 'id'))
+        self.assertTrue(hasattr(self.user, 'created_at'))
+        self.assertTrue(hasattr(self.user, 'updated_at'))
