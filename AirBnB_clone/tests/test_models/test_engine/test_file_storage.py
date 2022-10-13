7cc4b920fa4fff372875b1491977a2196713da0a
noemiepham <huynhtieuquy@gmail.com>
2022-10-13 11:34:15 +0200

    test file_path

--- AirBnB_clone/tests/test_models/test_engine/test_file_storage.py
+++ AirBnB_clone/tests/test_models/test_engine/test_file_storage.py
@@ -12,6 +12,11 @@ class TestFileStorage(unittest.TestCase):
         storage = FileStorage()
         self.assertIsInstance(storage, FileStorage)
 
+    def test_file_path(self):
+        """Test __file path is exited"""
+        path = FileStorage._FileStorage__file_path
+        self.assertEqual(str, type(path))
+
 
 if __name__ == "__main__":
     unittest.main()
