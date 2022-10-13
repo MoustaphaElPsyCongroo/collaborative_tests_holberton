1d5ff24fc10072357be4717c028c77d01c0dac5d
noemiepham <huynhtieuquy@gmail.com>
2022-10-13 14:15:31 +0200

    test_all()

--- AirBnB_clone/tests/test_models/test_engine/test_file_storage.py
+++ AirBnB_clone/tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 "Unit tests for FileStorage class"
 import unittest
 from models.engine.file_storage import FileStorage
+import os
 
 
 class TestFileStorage(unittest.TestCase):
@@ -17,6 +18,17 @@ class TestFileStorage(unittest.TestCase):
         path = FileStorage._FileStorage__file_path
         self.assertEqual(str, type(path))
 
+    def test_object(self):
+        """Test __object is type dict after deserialization object - dict"""
+        object_dict = FileStorage._FileStorage__objects
+        self.assertEqual(dict, type(object_dict))
+
+    def test_all(self):
+        """Test FileStorage: all()"""
+        """file is not exit"""
+        dict_return = {}
+        FileStorage.all(None)
+        self.assertTrue(os.path.isfile('file.json'))
 
 if __name__ == "__main__":
     unittest.main()
